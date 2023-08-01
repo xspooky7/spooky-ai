2d8a1d98aa74b711eb2cdbb22d4236e4dcfd06ca
7c_y <c.eissler@123@gmail.com>
2023-08-01 22:17:28 +0200

    fix: typo

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ import { CrispProvider } from '@/components/crisp-provider'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Spooy AI',
+  title: 'SpookyAI',
   description: 'All in one AI Toolkit',
 }
 
